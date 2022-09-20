e5557e6b1703aa2b046049216b340a0846af8b31
Chris Thompson <g0kla@arrl.net>
2022-09-20 16:41:20 -0400

    Need to build Release version to update its makefile

--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -5,16 +5,19 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../src/cmd_console.c \
+../src/config.c \
 ../src/gpio_interface.c \
 ../src/main.c 
 
 C_DEPS += \
 ./src/cmd_console.d \
+./src/config.d \
 ./src/gpio_interface.d \
 ./src/main.d 
 
 OBJS += \
 ./src/cmd_console.o \
+./src/config.o \
 ./src/gpio_interface.o \
 ./src/main.o 
 
@@ -31,7 +34,7 @@ src/%.o: ../src/%.c src/subdir.mk
 clean: clean-src
 
 clean-src:
-	-$(RM) ./src/cmd_console.d ./src/cmd_console.o ./src/gpio_interface.d ./src/gpio_interface.o ./src/main.d ./src/main.o
+	-$(RM) ./src/cmd_console.d ./src/cmd_console.o ./src/config.d ./src/config.o ./src/gpio_interface.d ./src/gpio_interface.o ./src/main.d ./src/main.o
 
 .PHONY: clean-src
 
